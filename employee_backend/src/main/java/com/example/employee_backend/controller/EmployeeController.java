301281865554d99c8ed9f9c695a09ffea51328e8
akhil <akhil.paul@ymail.com>
2022-12-06 15:13:41 +0530

    Adds retrieve from database

--- employee_backend/src/main/java/com/example/employee_backend/controller/EmployeeController.java
+++ employee_backend/src/main/java/com/example/employee_backend/controller/EmployeeController.java
@@ -1,16 +1,20 @@
 package com.example.employee_backend.controller;
 
+import com.example.employee_backend.dao.EmployeeDao;
 import com.example.employee_backend.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao dao;
     @GetMapping("/")
     public String homePage(){return "Welcome to homepage";}
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String empAddPage(@RequestBody Employee e){
         System.out.println(e.getEmpCode());
@@ -19,6 +23,7 @@ public class EmployeeController {
         System.out.println(e.getDesignation().toString());
 
         System.out.println(e.getSalary());
+        dao.save(e);
         return "Add employee page";}
 
 //    @PostMapping("/search")
@@ -27,8 +32,12 @@ public class EmployeeController {
 //    @PostMapping("/edit")
 //    public String empEditPage(){return "Add edit page";}
 //
-//    @GetMapping("/viewAll")
-//    public String empViewPage(){return "Adds view All page";}
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewAll")
+    public List<Employee> viewAllPage()
+    {
+        return (List<Employee>) dao.findAll();
+    }
 //
 //    @PostMapping("/delete")
 //    public String empDeletePage(){return "Adds employee delete page";}
